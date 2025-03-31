4f829f8cc408f5e4b4ac04bebeee4ad47f34f922
Chaplin Artem <75485190+ArCheeq@users.noreply.github.com>
2025-03-31 15:57:47 +0300

    fix: added navigation for mousepad

--- src/features/InteractionPane/index.tsx
+++ src/features/InteractionPane/index.tsx
@@ -12,10 +12,10 @@ export default function InteractionPane({ children }: PropsWithChildren) {
     const scale = useStore((state) => state.InteractionPane.scale);
     const setScale = useStore((state) => state.InteractionPane.setScale);
 
-    // const [initialDistanceBetweenFingers, setInitialDistanceBetweenFingers] = useState(0);
-    // const [initialScale, setInitialScale] = useState(1);
-    // const [initialTouchCenter, setInitialTouchCenter] = useState({ x: 0, y: 0 });
-    // const [initialOffset, setInitialOffset] = useState({ x: 0, y: 0 });
+    const [initialDistanceBetweenFingers, setInitialDistanceBetweenFingers] = useState(0);
+    const [initialScale, setInitialScale] = useState(1);
+    const [initialTouchCenter, setInitialTouchCenter] = useState({ x: 0, y: 0 });
+    const [initialOffset, setInitialOffset] = useState({ x: 0, y: 0 });
     const [dragPositionStart, setDragPositionStart] = useState({ x: 0, y: 0 });
     const [dragPositionOffsetStart, setDragPositionOffsetStart] = useState({ x: 0, y: 0 });
     const [isDragging, setIsDragging] = useState(false);
@@ -45,60 +45,90 @@ export default function InteractionPane({ children }: PropsWithChildren) {
         }
     }
 
-    // function handleTouchStart(event: TouchEvent<HTMLDivElement>) {
-    //     if (event.touches.length >= 2) {
-    //         const distanceX = event.touches[1].clientX - event.touches[0].clientX;
-    //         const distanceY = event.touches[1].clientY - event.touches[0].clientY;
-    //         const initialDistance = Math.sqrt(distanceX * distanceX + distanceY * distanceY);
-    //         setInitialDistanceBetweenFingers(initialDistance);
-    //         const centerX = event.touches[0].clientX + (event.touches[1].clientX - event.touches[0].clientX) / 2;
-    //         const centerY = event.touches[0].clientY + (event.touches[1].clientY - event.touches[0].clientY) / 2;
-    //         setInitialTouchCenter({
-    //             x: centerX,
-    //             y: centerY,
-    //         });
-    //         setInitialScale(scale);
-    //         setInitialOffset({ ...offset });
-    //     }
-    // }
+    function handleTouchStart(event: TouchEvent<HTMLDivElement>) {
+        if (event.touches.length >= 2) {
+            const distanceX = event.touches[1].clientX - event.touches[0].clientX;
+            const distanceY = event.touches[1].clientY - event.touches[0].clientY;
+            const initialDistance = Math.sqrt(distanceX * distanceX + distanceY * distanceY);
+            setInitialDistanceBetweenFingers(initialDistance);
+            const centerX = event.touches[0].clientX + (event.touches[1].clientX - event.touches[0].clientX) / 2;
+            const centerY = event.touches[0].clientY + (event.touches[1].clientY - event.touches[0].clientY) / 2;
+            setInitialTouchCenter({
+                x: centerX,
+                y: centerY,
+            });
+            setInitialScale(scale);
+            setInitialOffset({ ...offset });
+        }
+    }
 
     /*
      * Handles two finger actions: Pinch/zoom + two finger move
      * The action is initiated by handleTouchStart, and subsequently the following code is called
      * while the fingers are on the screen.
      */
-    // function handleTouchMove(event: TouchEvent<HTMLDivElement>) {
-    //     if (event.touches.length >= 2) {
-    //         //The following calculates how much the scale must changes (given the change in distance between the fingers)
-    //         const distanceX = event.touches[1].clientX - event.touches[0].clientX;
-    //         const distanceY = event.touches[1].clientY - event.touches[0].clientY;
-    //         const currentDistanceBetweenFingers = Math.sqrt(distanceX * distanceX + distanceY * distanceY);
-    //         const scaleChange = currentDistanceBetweenFingers / initialDistanceBetweenFingers;
-    //         const newScale = initialScale * scaleChange;
+    function handleTouchMove(event: TouchEvent<HTMLDivElement>) {
+        if (event.touches.length >= 2) {
+            //The following calculates how much the scale must changes (given the change in distance between the fingers)
+            const distanceX = event.touches[1].clientX - event.touches[0].clientX;
+            const distanceY = event.touches[1].clientY - event.touches[0].clientY;
+            const currentDistanceBetweenFingers = Math.sqrt(distanceX * distanceX + distanceY * distanceY);
+            const scaleChange = currentDistanceBetweenFingers / initialDistanceBetweenFingers;
+            const newScale = initialScale * scaleChange;
+            setScale(newScale);
+
+            //The following adapts the offset in such a way that we zoom in on the center point between the fingers
+            //1. Take the original center, and convert it from screen coordinate space to image coordinate space
+            const paneCoordinateX = (initialTouchCenter.x - initialOffset.x) / initialScale;
+            const paneCoordinateY = (initialTouchCenter.y - initialOffset.y) / initialScale;
+            //2. Calculate where this center would end up, given the new scale
+            const wouldBeNewScreenPositionX = paneCoordinateX * newScale + initialOffset.x;
+            const wouldBeNewScreenPositionY = paneCoordinateY * newScale + initialOffset.y;
+            //3. Calculate the different between the two (we want it to be zero)
+            const offsetAdjustmentX1 = initialTouchCenter.x - wouldBeNewScreenPositionX;
+            const offsetAdjustmentY1 = initialTouchCenter.y - wouldBeNewScreenPositionY;
+
+            //In addition, the following adapts the offset when both fingers move in a particular direction
+            //This way you can pinch/zoom and move the canvas at the same time
+            const currentCenterX = event.touches[0].clientX + (event.touches[1].clientX - event.touches[0].clientX) / 2;
+            const currentCenterY = event.touches[0].clientY + (event.touches[1].clientY - event.touches[0].clientY) / 2;
+            const offsetAdjustmentX2 = currentCenterX - initialTouchCenter.x;
+            const offsetAdjustmentY2 = currentCenterY - initialTouchCenter.y;
+
+            //Finally, apply both offset adjustments
+            setOffset({
+                x: initialOffset.x + offsetAdjustmentX1 + offsetAdjustmentX2,
+                y: initialOffset.y + offsetAdjustmentY1 + offsetAdjustmentY2,
+            });
+        }
+    }
+
+    // function handleWheel(event: WheelEvent<HTMLDivElement>) {
+    //     const initialScale = scale;
+    //     const initialOffset = { ...offset };
+    //
+    //     if (event.ctrlKey) {
+    //         event.preventDefault();
+    //
+    //         let newScale;
+    //         if (event.deltaY > 0) newScale = initialScale * (1 - SCROLL_WHEEL_FACTOR);
+    //         else newScale = initialScale * (1 + SCROLL_WHEEL_FACTOR);
     //         setScale(newScale);
-    //
-    //         //The following adapts the offset in such a way that we zoom in on the center point between the fingers
-    //         //1. Take the original center, and convert it from screen coordinate space to image coordinate space
-    //         const paneCoordinateX = (initialTouchCenter.x - initialOffset.x) / initialScale;
-    //         const paneCoordinateY = (initialTouchCenter.y - initialOffset.y) / initialScale;
-    //         //2. Calculate where this center would end up, given the new scale
-    //         const wouldBeNewScreenPositionX = paneCoordinateX * newScale + initialOffset.x;
-    //         const wouldBeNewScreenPositionY = paneCoordinateY * newScale + initialOffset.y;
-    //         //3. Calculate the different between the two (we want it to be zero)
-    //         const offsetAdjustmentX1 = initialTouchCenter.x - wouldBeNewScreenPositionX;
-    //         const offsetAdjustmentY1 = initialTouchCenter.y - wouldBeNewScreenPositionY;
-    //
-    //         //In addition, the following adapts the offset when both fingers move in a particular direction
-    //         //This way you can pinch/zoom and move the canvas at the same time
-    //         const currentCenterX = event.touches[0].clientX + (event.touches[1].clientX - event.touches[0].clientX) / 2;
-    //         const currentCenterY = event.touches[0].clientY + (event.touches[1].clientY - event.touches[0].clientY) / 2;
-    //         const offsetAdjustmentX2 = currentCenterX - initialTouchCenter.x;
-    //         const offsetAdjustmentY2 = currentCenterY - initialTouchCenter.y;
-    //
-    //         //Finally, apply both offset adjustments
+    //         //1. Take the center, and convert it from screen coordinate space to image coordinate space.
+    //         const imageCoordinateX = (event.clientX - initialOffset.x) / initialScale;
+    //         const imageCoordinateY = (event.clientY - initialOffset.y) / initialScale;
+    //         //2. The following adapts the offset in such a way that we zoom in on point where the scroll wheel is used.
+    //         const newOffsetX = initialOffset.x + initialScale * imageCoordinateX - newScale * imageCoordinateX;
+    //         const newOffsetY = initialOffset.y + initialScale * imageCoordinateY - newScale * imageCoordinateY;
+    //         //Finally, apply the adjustment
     //         setOffset({
-    //             x: initialOffset.x + offsetAdjustmentX1 + offsetAdjustmentX2,
-    //             y: initialOffset.y + offsetAdjustmentY1 + offsetAdjustmentY2,
+    //             x: newOffsetX,
+    //             y: newOffsetY,
+    //         });
+    //     } else {
+    //         setOffset({
+    //             x: initialOffset.x,
+    //             y: initialOffset.y - event.deltaY,
     //         });
     //     }
     // }
@@ -114,20 +144,17 @@ export default function InteractionPane({ children }: PropsWithChildren) {
             if (event.deltaY > 0) newScale = initialScale * (1 - SCROLL_WHEEL_FACTOR);
             else newScale = initialScale * (1 + SCROLL_WHEEL_FACTOR);
             setScale(newScale);
-            //1. Take the center, and convert it from screen coordinate space to image coordinate space.
             const imageCoordinateX = (event.clientX - initialOffset.x) / initialScale;
             const imageCoordinateY = (event.clientY - initialOffset.y) / initialScale;
-            //2. The following adapts the offset in such a way that we zoom in on point where the scroll wheel is used.
             const newOffsetX = initialOffset.x + initialScale * imageCoordinateX - newScale * imageCoordinateX;
             const newOffsetY = initialOffset.y + initialScale * imageCoordinateY - newScale * imageCoordinateY;
-            //Finally, apply the adjustment
             setOffset({
                 x: newOffsetX,
                 y: newOffsetY,
             });
         } else {
             setOffset({
-                x: initialOffset.x,
+                x: initialOffset.x - event.deltaX,
                 y: initialOffset.y - event.deltaY,
             });
         }
@@ -166,8 +193,8 @@ export default function InteractionPane({ children }: PropsWithChildren) {
             onPointerDown={handlePointerDown}
             onPointerUp={handlePointerUp}
             onPointerMove={handlePointerMove}
-            // onTouchStart={handleTouchStart}
-            // onTouchMove={handleTouchMove}
+            onTouchStart={handleTouchStart}
+            onTouchMove={handleTouchMove}
             onWheel={handleWheel}
             style={{
                 touchAction: "none",
