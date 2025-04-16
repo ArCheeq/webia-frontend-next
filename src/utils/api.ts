c8e07a7116b6d0348f7b7957dff1e871e1322aee
Chaplin Artem <75485190+ArCheeq@users.noreply.github.com>
2025-04-16 12:48:35 +0300

    uptodate

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -3,6 +3,7 @@ import { CONFIG } from "@/constants/config";
 
 export const api = axios.create({
     baseURL: CONFIG.servers.api,
+    timeout: 0,
     headers: {
         'ngrok-skip-browser-warning': true,
     }
